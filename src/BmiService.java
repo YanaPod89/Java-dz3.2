f8f4cd7d08bf18517f97d27f374e4fef90467798
Yana <podolskaya_22@mail.ru>
2024-07-12 00:00:43 +0500

    format

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate (int weight, float height) {
-        float bmi= weight/(height*height);
+    public int calculate(int weight, float height) {
+        float bmi = weight / (height * height);
         return (int) bmi;
 
     }
